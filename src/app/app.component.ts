6fe55edb6acd0931ba20c1c1743f0e1438388241
Alejandro Marín Oliva <amarin@profile.es>
2024-09-11 15:29:25 +0200

    Initial folder structure and translation system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSwitchService } from './core/language-switch/language-switch.service';
+import { NavbarComponent } from './core/language-switch/language-switch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-18-archetype';
+  languageService = inject(LanguageSwitchService);
+  constructor() {
+    this.languageService.setLanguage('es')
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
+import { initializeTranslation, provideTranslation } from './config/httpLoaderFactory';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([TranslateModule.forRoot(provideTranslation())]),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeTranslation,
+      multi: true,
+      deps: [TranslateService],
+    },
+  ]
 };
